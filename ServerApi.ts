33b61f68467cde7d5bf12ad3e9728f4dc8fc3c7c
jakab.wolosz <jakab.wolosz@bragg.group>
2026-09-07 23:11:58 +0200

    update port

--- ServerApi.ts
+++ ServerApi.ts
@@ -5,7 +5,7 @@ export class ServerApi {
 
     constructor() {
         this.server = new WebSocketServer({
-            port: 3001
+            port: Number(process.env.PORT); //3001
         });
 
         console.log("server api is ready");
